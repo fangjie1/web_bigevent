5ecb5917f88153fc32ce9febaf266c3576d3f586
fangjie <857516512@qq.com>
2021-11-27 16:28:14 +0800

    完成了文章管理相关功能的开发 git commit -a-m完成了文章管理相关功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ $(function () {
             // 1. 清空本地存储中的 token
             localStorage.removeItem('token')
             // 2. 重新跳转到登录页面
-            location.href = '/login.html'
+            location.href = './login.html'
             // 关闭 confirm 询问框
             layer.close(index)
         })
@@ -29,7 +29,7 @@ function getUserInfo() {
             }
             // 调用 renderAvatar 渲染用户的头像
             renderAvatar(res.data)
-        },
+        }
     })
 }
 function renderAvatar(user) {
--- assets/js/login.js
+++ assets/js/login.js
@@ -46,7 +46,7 @@ $(function () {
                 }
                 layer.msg('登录成功')
                 localStorage.setItem('token', res.token)
-                location.href = '/index.html'
+                location.href = './index.html'
             }
         })
     })
